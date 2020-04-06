8aeca6ee044f6c22ce3094ae88f9b593e4efb1b0
Wojciech Michałowski <wojtek1214.99@wp.pl>
2020-04-06 15:06:22 +0200

    end

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { LatLngLiteral } from "leaflet";
+
+import io from "socket.io-client";
+
+type MarkerData = {
+  id: string;
+  position: LatLngLiteral;
+  text: string;
+};
+
+const App = () => {
+  const [center] = useState<LatLngLiteral>({
+    lat: 52.768333,
+    lng: 23.191944,
+  });
+  const [zoom] = useState(13);
+  const [markers, setMarkers] = useState<MarkerData[]>([]);
+
+  useEffect(() => {
+    const socket = io("localhost:8081/map");
+    socket.on("markers", (markers: MarkerData[]) => {
+      console.log("blabla");
+      setMarkers(markers);
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map center={center} zoom={zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {markers.map((marker) => (
+          <Marker
+            riseOnHover
+            key={marker.id}
+            position={marker.position}
+            opacity={0.6}
+          >
+            <Popup>{marker.text}</Popup>
+          </Marker>
+        ))}
+      </Map>
     </div>
   );
-}
+};
 
 export default App;
+
